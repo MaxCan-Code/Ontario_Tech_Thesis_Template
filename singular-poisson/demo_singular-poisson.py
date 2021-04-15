c225b49951f031a5dc18fecb41459e45789675a5
MaxCan-Code <35848843+MaxCan-Code@users.noreply.github.com>
2021-04-15 09:19:12 -0400

    point-wise error convergence

--- singular-poisson/demo_singular-poisson.py
+++ singular-poisson/demo_singular-poisson.py
@@ -185,8 +185,6 @@ def coeff(mesh, obj):
 
 
 def test_pert(obj_geom, n_cells, n_refns, R, points):
-def test_singular_poisson():
-    "test and plot"
 
     # source :math:`f` and boundary normal derivative :math:`g`	
     f = Constant(0)
@@ -214,7 +212,6 @@ def test_singular_poisson():
 
 
 def plot_and_save(u_obj, u_empty):
-    # and plot the solution ::
     "plot and save the solution"
 
     u_diff = u_obj - u_empty
@@ -229,12 +226,35 @@ def plot_and_save(u_obj, u_empty):
 
 
 def main():
-    plane_size = (4, 10)
-    p_mag = mk_plane_source(plane_size)
-    # p_mag = [(Point(), 20)]
+    # plane_size = (4, 10)
+    # ps_plane = mk_plane_source(plane_size)
+    # 
+    # u_obj, u_empty = test_pert("box", 30, 2, 50, ps_plane)
+    # plot_and_save(u_obj, u_empty)
 
-    u_obj, u_empty = test_pert("box", 30, 2, 50, p_mag)
-    plot_and_save(u_obj, u_empty)
+    q = 20
+    ps_pt = [(Point(), q)]
+    # u_empty_1ps_lst = [test_pert("box", 32, 2, i*10, ps_pt)[1] for i in range(2,6)]
+    u_empty_1ps_lst = [test_pert("box", 32, i, 50, ps_pt)[1] for i in range(5)]
+    # u_empty_1ps_lst = [test_pert("box", i, 2, 50, ps_pt)[1] for i in [16, 32, 64, 128, 256]]
+
+    u_e = Expression(
+        f"""
+        {q}/(4 * pi * sqrt(
+        pow(x[0], 2)+
+        pow(x[1], 2)+
+        pow(x[2], 2)))
+        """,
+        degree=2
+    )
+
+    pt_ = (0, 0, 7)
+    x0 = np.array(pt_)
+    for u_empty_ in u_empty_1ps_lst:
+        V = u_empty_.function_space()
+        dof_coords = V.tabulate_dof_coordinates()
+        dof = np.argmin(np.linalg.norm(dof_coords - x0, axis=1))
+        node_ = dof_coords[dof]
+        print(f"{u_empty_(node_) - u_e(node_)}   {u_empty_(pt_) - u_e(pt_)} {node_}") 
 
 
-main()
